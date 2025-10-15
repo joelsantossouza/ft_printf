54ebd67ce78fcc8cb722f5138e62b38dc5119aca
joel <joelsantossouza08@gmail.com>
2025-10-15 11:51:56 +0100

    For now, im going to focus on finish the skeleton of project, after i will improve details like less system calls

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 20:28:51 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/15 09:23:08 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/15 11:50:07 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,41 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+#define STDOUT	1
+
+static void	add_bytes(ssize_t add, int *store)
+{
+	if (*store == -1)
+		return ;
+	if (add >= 0)
+		*store += add;
+	else
+		*store = -1;
+}
+
 int	ft_printf(const char *format, ...)
 {
 	t_spec		spec;
 	va_list		args;
 	int			nbytes;
+	const char	*spec_str;
 
 	nbytes = 0;
 	va_start(args, format);
 	while (*format && nbytes >= 0)
 	{
 		if (*format != '%' || *++format == '%')
-
+			add_bytes(ft_putchar_fd(*format, STDOUT), &nbytes);
+		else
+		{
+			ft_memset(&spec, 0, sizeof(t_spec));
+			parse_flags(format, &spec, &format);
+			parse_width(format, &spec, &format);
+			parse_precision(format, &spec, &format);
+			parse_length(format, &spec, &format);
+			spec_str = get_spec_str(format, &spec, &format);
+			add_bytes(print_spec(spec_str, spec, STDOUT), &nbytes);
+		}
 	}
 	va_end(args);
 	return (nbytes);
