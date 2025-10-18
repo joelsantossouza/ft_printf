b06b8f3614fdc142c13eee7db6c1f1ead740ec5f
joel <joelsantossouza08@gmail.com>
2025-10-18 14:21:09 +0100

    Fix bugs of %p, that was ignoring precision and in the special case of NULL wanst ignoring precision

--- srcs/types_config.c
+++ srcs/types_config.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:07:10 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/18 14:03:41 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/18 14:19:24 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,13 +82,18 @@ int	chr_config(va_list args, t_spec *spec)
 
 int	ptr_config(va_list args, t_spec *spec)
 {
+	const char	zero_pad = (spec->flags & ZERO_PAD) && (spec->flags & RIGHT_JUSTIFY);
+
 	spec->pad = ' ';
 	spec->str = va_arg(args, void *);
 	if (!spec->str)
 	{
+		spec->precision = 0;
 		spec->str = "(nil)";
 		return (5);
 	}
 	ft_strlcpy(spec->prefix, "0x", sizeof(spec->prefix));
+	if (zero_pad || (spec->flags & PRECISION))
+		spec->pad = '0';
 	return (ft_static_ultoa_base((size_t) spec->str, &spec->str, LHEX));
 }
