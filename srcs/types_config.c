696f0f00402c69e9bb54bfe0fa724cd6f2d76d47
joel <joelsantossouza08@gmail.com>
2025-10-20 17:51:26 +0100

    Fix bug of %s

--- srcs/types_config.c
+++ srcs/types_config.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:07:10 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/20 11:42:22 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/20 17:50:43 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,10 +62,7 @@ int	str_config(va_list args, t_spec *spec)
 	spec->precision = 0;
 	spec->str = va_arg(args, char *);
 	if (!spec->str)
-	{
 		spec->str = "(null)";
-		return (6);
-	}
 	if (spec->flags & PRECISION)
 		return (ft_strnlen(spec->str, precision));
 	return (ft_strlen(spec->str));
